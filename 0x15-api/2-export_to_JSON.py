f0623c6dde29bf2d2d13ab3b05432b537c56e0f6
Heba Omar <jtvonline@yahoo.com>
2023-10-23 20:08:04 +0300

    Using what you did in the task #0, extend your Python script to export data in the JSON format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Export to CSV """
+""" Export to JSON """
 
 import json
 import requests
@@ -7,14 +7,13 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    """Export to CSV """
+    """Export to JSON """
 
     # Fetching the employee's name
     url = "https://jsonplaceholder.typicode.com/"
     user_id = argv[1]
     user = requests.get(url + "users/{}".format(user_id)).json()
     tasks = requests.get(url + "todos", params={"userId": user_id}).json()
-    res = requests.get(url)
 
     # Saving data as a file
     with open("{}.json".format(user_id), "w") as file:
@@ -22,5 +21,5 @@ if __name__ == "__main__":
             json.dump({user_id: [{
              "task": task.get('title'),
              "completed": task.get('completed'),
-             "username": task.get('username')
+             "username": user.get('username')
             }]}, file)

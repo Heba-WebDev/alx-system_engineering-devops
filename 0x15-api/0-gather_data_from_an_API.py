4fdf5176f16daf7fdce421dcd78342e1bd65a291
Heba Omar <jtvonline@yahoo.com>
2023-10-23 19:14:42 +0300

    Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
+"""Gather data from an API"""
+
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    """ Gather data from an API """
-    # Fetching the user data and getting the name
+
+if __name__ == "__main__":
+    """Gather data from an API"""
+
+    # Fetching the employee's name
     url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    req = requests.get(url)
-    name = req.json().get('name')
+    res = requests.get(url)
+
+    name = res.json().get('name')
+
     # Fetching the todos
     url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        argv[1])
-    req = requests.get(url)
-    todos = req.json()
-    len = len(todos)
-    completed = [task for task in todos if task.get('completed') is True]
-    print("Employee {} is done with tasks ({}/{})".format(
-        name, completed, len))
-    for task in completed:
+            argv[1]
+    )
+    res = requests.get(url)
+
+    tasks = res.json()
+    total = len(tasks)
+    done = [task for task in tasks if task.get('completed') is True]
+    print("Employee {} is done with tasks({}/{}):".format(name, len(done),
+                                                          total))
+    for task in done:
         print("\t {}".format(task.get('title')))

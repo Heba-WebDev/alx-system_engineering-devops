ecc3735025d210ee6dee1baaaaef94ff6cd9922c
Heba Omar <jtvonline@yahoo.com>
2023-11-07 21:38:15 +0300

    Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-
-""" Write a recursive function that queries the
-Reddit API and returns a list containing the titles
-of all hot articles for a given subreddit.
-If no results are found for the given subreddit,
-the function should return None. """
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
-    """ Returns a list containing the titles of all
-    hot articles for a given subreddit """
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
